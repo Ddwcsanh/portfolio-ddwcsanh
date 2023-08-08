a5e2f2dadbcd16b8bd17a1a15374e15ed45c13d0
ddwcsanh <ledoducanh03@gmail.com>
2023-08-09 02:44:34 +0700

    fix vercel

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect } from 'react';
-import Navigation from './Navigation';
-import About from './About';
-import Footer from './Footer';
-import Home from './Home';
-import Resume from './Resume';
-import Projects from './Projects';
-import Contact from './Contact';
-import { useMediaQuery, useTheme } from '@mui/material';
-import ErrorPage from './ErrorPage';
+import { useEffect } from 'react'
+import Navigation from './Navigation'
+import About from './About'
+import Footer from './Footer'
+import Home from './Home'
+import Resume from './Resume'
+import Projects from './Projects'
+import Contact from './Contact'
+import { useMediaQuery, useTheme } from '@mui/material'
+import ErrorPage from './ErrorPage'
 
 const Main = () => {
-  const theme = useTheme();
-  const md = useMediaQuery(theme.breakpoints.down('md'));
+  const theme = useTheme()
+  const md = useMediaQuery(theme.breakpoints.down('md'))
 
   useEffect(() => {
     const scrollToTop = () => {
       if (window.scrollY !== 0) {
-        window.scrollBy(0, -100);
-        requestAnimationFrame(scrollToTop);
+        window.scrollBy(0, -100)
+        requestAnimationFrame(scrollToTop)
       }
-    };
+    }
 
-    scrollToTop();
-  }, []);
+    scrollToTop()
+  }, [])
 
   return (
     <>
@@ -40,7 +40,7 @@ const Main = () => {
         </>
       )}
     </>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
